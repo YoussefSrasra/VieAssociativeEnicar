38acc51f69e9796e7b9f2a20a463e098e6cbb1fd
souissiarij <souissiarij187@gmail.com>
2025-04-10 21:03:19 +0100

    front done

---
+++ Front/src/app/entretiens/entretien.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+export interface Entretien {
+  id: number;
+  enrollment: { prenom: string; nom: string };
+  dateEntretien: string;
+  heureEntretien: string;
+  statut: string;
+  resultat: string;
+  confirmation: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EntretienService {
+  private apiUrl = 'http://localhost:8080/api/entretiens';
+
+  constructor(private http: HttpClient) { }
+
+  getEntretiens(): Observable<Entretien[]> {
+    return this.http.get<Entretien[]>(this.apiUrl);
+  }
+
+  updateEntretien(id: number, entretien: Entretien): Observable<Entretien> {
+    return this.http.put<Entretien>(`${this.apiUrl}/${id}`, entretien);
+  }
+}
--- Front/src/app/entretiens/entretiens.component.ts
+++ Front/src/app/entretiens/entretiens.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { EntretienService } from './entretien.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-entretiens',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './entretiens.component.html',
   styleUrl: './entretiens.component.scss'
 })
-export class EntretiensComponent {
 
+
+export class EntretiensComponent implements OnInit {
+  entretiens: any[] = [];
+
+  constructor(private entretienService: EntretienService) { }
+
+  ngOnInit(): void {
+    this.loadEntretiens();
+  }
+
+  loadEntretiens(): void {
+    this.entretienService.getEntretiens().subscribe({
+      next: (data) => this.entretiens = data,
+      error: (err) => console.error('Erreur:', err)
+    });
+  }
+
+  saveEntretien(entretien: any): void {
+    this.entretienService.updateEntretien(entretien.id, entretien).subscribe({
+      next: () => alert('Modifications sauvegardées!'),
+      error: (err) => console.error('Erreur:', err)
+    });
+  }
 }
